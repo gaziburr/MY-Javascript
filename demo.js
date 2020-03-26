a40718ec5ba9fd4cbe931fd9116fe1196f492e5a
GAZIBUR <gaziburrahman1999@gmail.com>
2020-03-26 13:28:50 +0530

    something rest aggain

--- demo.js
+++ demo.js
@@ -36,6 +36,12 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     scores[activePlayer] += roundscore
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
         //next player
+
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = '--WINNER!--';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner')
+
+    }
     nextPlayer()
 })
 
