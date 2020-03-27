205af8bea842bab815fcff816ae1fe3b7415897d
GAZIBUR <gaziburrahman1999@gmail.com>
2020-03-27 16:32:24 +0530

    few style added

--- demo.js
+++ demo.js
@@ -8,6 +8,7 @@ document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0'
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
+document.querySelector('.player-score').style.color = 'blue'
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     //1. Random number...
@@ -49,10 +50,21 @@ function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundscore = 0;
     document.querySelector('.dice').style.display = 'none';
-
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 }
 //something rest
+document.querySelector('.btn-new').addEventListener('click', function() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundscore = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle(null)
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+})
