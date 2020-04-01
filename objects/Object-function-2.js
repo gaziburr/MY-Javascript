c35d071545ad0b42dc5e88cb0af504b080ec178e
GAZIBUR <gaziburrahman1999@gmail.com>
2020-04-01 08:09:37 +0530

    object-function-2 is Modified

--- objects/Object-function-2.js
+++ objects/Object-function-2.js
@@ -1,13 +1,10 @@
 var Gazibur = {
-    firstName: "Gazibur",
-    lastName: " Rahman",
-    birthYear: 1999,
     family: ["Mofiz uddin", "Johura maya", "jayeda", "Asmina", "Halima", "Jiabur", "Motibur", "Hifjur", "Azizul", "Gazibur "],
     familyBirthYear: [1959, 1967, 1982, 1985, 1987, 1986, 1990, 1992, 1995, 1999],
     familyAges: [], //This array is filled by callback function(function as an arguement) below
     pensiontime: [],
-    Job: "student",
-    isMarried: []
+    isMarried: [],
+    Job: "student"
 };
 // call back function
 
@@ -38,7 +35,38 @@ function pension(index) {
     var cal = 60 - index;
     return cal;
 }
+
+
+
+
+var isMarried = Gazibur.isMarried
+
+function Married(arr, fn) {
+    for (var i = 0; i < ages.length; i++) {
+        isMarried.push(fn(arr[i]))
+    }
+}
+
+function Married(arr, fn) {
+    for (var i = 0; i < ages.length; i++) {
+        isMarried.push(fn(arr[i]))
+    }
+}
+
+
+
+
+function name(el) {
+    if (el >= 25) {
+        return true
+    } else {
+        return false
+    }
+}
 var ages = Gazibur.familyAges
 pensionyear(ages, pension);
+Married(ages, name)
+console.log(Gazibur.isMarried)
 console.log(Gazibur.pensiontime)
+console.log(ages)
 console.table(Gazibur)
